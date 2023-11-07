901817da4790b9f15a823082fca14820037691d5
TheCodester2k19 <150153602+TheCodester2k19@users.noreply.github.com>
2023-11-07 23:26:44 +0530

    Update WC_mapper.java

--- WC_mapper.java
+++ WC_mapper.java
@@ -1,25 +1,30 @@
-package com.javatpoint;  
-  
-import java.io.IOException;    
-import java.util.StringTokenizer;    
-import org.apache.hadoop.io.IntWritable;    
-import org.apache.hadoop.io.LongWritable;    
-import org.apache.hadoop.io.Text;    
-import org.apache.hadoop.mapred.MapReduceBase;    
-import org.apache.hadoop.mapred.Mapper;    
-import org.apache.hadoop.mapred.OutputCollector;    
-import org.apache.hadoop.mapred.Reporter;    
-public class WC_Mapper extends MapReduceBase implements Mapper<LongWritable,Text,Text,IntWritable>{    
-    private final static IntWritable one = new IntWritable(1);    
-    private Text word = new Text();    
-    public void map(LongWritable key, Text value,OutputCollector<Text,IntWritable> output,     
-           Reporter reporter) throws IOException{    
-        String line = value.toString();    
-        StringTokenizer  tokenizer = new StringTokenizer(line);    
-        while (tokenizer.hasMoreTokens()){    
-            word.set(tokenizer.nextToken());    
-            output.collect(word, one);    
-        }    
-    }    
-    
-} 
+// Importing libraries
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+public class WCMapper extends MapReduceBase implements Mapper<LongWritable,
+												Text, Text, IntWritable> {
+
+	// Map function
+	public void map(LongWritable key, Text value, OutputCollector<Text, 
+				IntWritable> output, Reporter rep) throws IOException
+	{
+
+		String line = value.toString();
+
+		// Splitting the line on spaces
+		for (String word : line.split(" ")) 
+		{
+			if (word.length() > 0)
+			{
+				output.collect(new Text(word), new IntWritable(1));
+			}
+		}
+	}
+}
